b39d2d482d5844a4eb50c6359278c6174f9256db
Tushar Sharma <127395795+tr3atm3@users.noreply.github.com>
2024-12-09 17:04:56 +0530

    Add files via upload

--- WhatsappWidget.js
+++ WhatsappWidget.js
@@ -1,25 +1,122 @@
-// WhatsAppChatButton.js
-function createWhatsAppButton(phoneNumber) {
-  const button = document.createElement("a");
-  button.href = `https://wa.me/${phoneNumber}`;
-  button.target = "_blank";
-  button.style.position = "fixed";
-  button.style.bottom = "20px";
-  button.style.right = "20px";
-  button.style.backgroundColor = "#25D366";
-  button.style.borderRadius = "50%";
-  button.style.padding = "15px";
-  button.style.boxShadow = "0 4px 6px rgba(0,0,0,0.1)";
-  button.style.zIndex = "1000";
+/* eslint-disable no-restricted-globals */
+try {
+  const widgetId = document
+    .getElementById("engageto-wa-widget")
+    .getAttribute("widget-id");
+  if (!widgetId || widgetId == null) {
+    throw new Error("Widget is invalid");
+  }
+  // console.log("Loading script");
+  const s = document.createElement("script");
+  s.src = "https://tr3atm3.github.io/whatsappchatwidget/widgetPluginDev.js";
+  // s.src = "http://localhost:8000/widget-plugin-dev.js";
+  s.type = "text/javascript";
+  s.async = true;
+  s.id = "df-script";
+  // console.log(s);
 
-  const icon = document.createElement("img");
-  icon.src = "https://upload.wikimedia.org/wikipedia/commons/6/6b/WhatsApp.svg";
-  icon.alt = "Chat with us on WhatsApp";
-  icon.style.width = "40px";
-  icon.style.height = "40px";
+  // Load Poppins font;
+  const url =
+    "https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap";
+  let link = document.createElement("link");
+  link.rel = "stylesheet";
+  link.href = url;
+  document.getElementsByTagName("head")[0].appendChild(link);
 
-  button.appendChild(icon);
-  document.body.appendChild(button);
+  // Fetch widget details
+  var aisensyLink = null;
+  var aisensyWidgetOptions = null;
+
+  var xhr = new XMLHttpRequest();
+  xhr.open(
+    "GET",
+    "https://connect.engagetoq.in/api/Widgets/get" + widgetId,
+    true
+  );
+  // xhr.open("GET", "http://localhost:5000/widget-details/" + widgetId, true);
+  xhr.setRequestHeader("Content-Type", "application/json");
+  xhr.send();
+  xhr.onload = function () {
+    if (this.status === 200) {
+      var data = JSON.parse(this.responseText);
+      console.log(data);
+      if (data.link) {
+        aisensyLink = data.link;
+      }
+      if (aisensyLink != null) {
+        var btn = aisensyLink.whatsappBtnSettings;
+        aisensyWidgetOptions = {
+          linkid: aisensyLink.linkId,
+          phoneNumber: aisensyLink.phoneNumber,
+          variant: btn.variant,
+          btnBackground: btn.btnBackground,
+          btnCTA: btn.btnCTA,
+          mb: btn.mb,
+          ml: btn.ml,
+          mr: btn.mr,
+          borderRadius: btn.borderRadius,
+          prefilledMsg: aisensyLink.prefilledMsg,
+          position: btn.position,
+          brandName: btn.brandName,
+          brandSub: btn.brandSub,
+          brandColor: btn.brandColor,
+          brandImgUrl: btn.brandImgUrl,
+          widgetBtnCTA: btn.widgetBtnCTA,
+          openWidgetByDefault: btn.openWidgetByDefault,
+          openWidgetSessionWindow: btn.openWidgetSessionWindow,
+          onscreenMsg: btn.onscreenMsg,
+          onscreenImg: "",
+          widgetOnMobile: btn.widgetOnMobile,
+          personalizedUrls: btn.personalizedUrls,
+        };
+      } else {
+        // Req successful but empty data is returned
+        throw new Error("Invalid widget details");
+      }
+
+      s.onload = function () {
+        console.log("Loaded script");
+        console.log({ aisensyWidgetOptions });
+
+        if (aisensyWidgetOptions != null) {
+          console.log({ aisensyWidgetOptions });
+          window.CreateWhatsAppButtonAndWidget(aisensyWidgetOptions);
+        }
+
+        // Register event listener for url change
+        history.pushState = ((f) =>
+          function pushState() {
+            var ret = f.apply(this, arguments);
+            window.dispatchEvent(new Event("pushstate"));
+            window.dispatchEvent(new Event("locationchange"));
+            return ret;
+          })(history.pushState);
+
+        history.replaceState = ((f) =>
+          function replaceState() {
+            var ret = f.apply(this, arguments);
+            window.dispatchEvent(new Event("replacestate"));
+            window.dispatchEvent(new Event("locationchange"));
+            return ret;
+          })(history.replaceState);
+
+        window.addEventListener("popstate", () => {
+          window.dispatchEvent(new Event("locationchange"));
+        });
+
+        // Listen for url change
+        window.addEventListener("locationchange", () => {
+          console.log("Location changed");
+          if (aisensyWidgetOptions != null) {
+            window.CreateWhatsAppButtonAndWidget({ ...aisensyWidgetOptions });
+          }
+        });
+      };
+      document.body.appendChild(s);
+    } else {
+      console.log(`Error ${xhr.status}: ${xhr.statusText}`);
+    }
+  };
+} catch (err) {
+  console.log(err);
 }
-
-window.createWhatsAppButton = createWhatsAppButton;
