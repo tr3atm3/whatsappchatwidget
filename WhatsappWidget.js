ed75c870f0cdace01cccb372ab8d80ce2bc9bc7b
Tushar Sharma <127395795+tr3atm3@users.noreply.github.com>
2024-12-17 18:58:54 +0530

    Update WhatsappWidget.js

--- WhatsappWidget.js
+++ WhatsappWidget.js
@@ -11,9 +11,9 @@ try {
   }
   // console.log("Loading script");
   const s = document.createElement("script");
-  // s.src =
-     // "https://tr3atm3.github.io/whatsappchatwidget/widgetPluginDev.js";
-  s.src = "https://dev-engageto.s3.ap-south-1.amazonaws.com/whatsapp-widget-ui.js";
+  s.src =
+     "https://tr3atm3.github.io/whatsappchatwidget/widgetPluginDev.js";
+  // s.src = "https://dev-engageto.s3.ap-south-1.amazonaws.com/whatsapp-widget-ui.js";
   s.type = "text/javascript";
   s.async = true;
   s.id = "df-script";
